c97cb36fa2138c7e6e64f5005bfe981d9f28b131
Alireza Dastyar <alireza@dastyar.space>
2024-06-12 14:37:24 +0000

    Use DI container from app package

--- cmd/application/main.go
+++ cmd/application/main.go
@@ -1,54 +1,9 @@
 package main
 
 import (
-	"go.uber.org/fx"
-	"go.uber.org/fx/fxevent"
-
-	"order-service/internal/api"
 	"order-service/internal/app"
-	"order-service/internal/configuration"
-	"order-service/internal/database"
-	"order-service/internal/discovery"
-	"order-service/internal/message"
-	"order-service/internal/persistence"
-	"order-service/internal/service"
-	"order-service/internal/util"
 )
 
 func main() {
-	fx.New(
-		fx.Provide(configuration.NewConfig),
-		fx.Provide(app.NewLogger),
-		fx.Provide(app.NewFxLogger),
-		fx.Provide(app.ProvideEcho),
-		fx.Provide(app.NewAppSetupManager),
-		fx.Provide(discovery.NewServiceDiscovery),
-		fx.Provide(util.NewValidator),
-		fx.Provide(api.NewHTTPErrorHandler),
-		fx.Provide(database.NewDatabaseConnection),
-		fx.Provide(message.NewMessageQueueClient),
-		fx.Provide(persistence.NewOrderDAO),
-		fx.Provide(persistence.NewPositionDAO),
-		fx.Provide(service.NewOrderService),
-		fx.Provide(service.NewPositionService),
-		fx.Provide(message.NewMessageHandler),
-		asHandler(api.NewHealthCheck),
-		asHandler(api.NewPositionHandler),
-		fx.Provide(fx.Annotate(
-			app.NewRestApp,
-			fx.ParamTags(`group:"handlers"`),
-		)),
-		fx.WithLogger(func(log app.FxLogger) fxevent.Logger {
-			return &log
-		}),
-		fx.Invoke(app.ManageLifeCycle),
-	).Run()
-}
-
-func asHandler(handler interface{}) fx.Option {
-	return fx.Provide(fx.Annotate(
-		handler,
-		fx.As(new(api.Handler)),
-		fx.ResultTags(`group:"handlers"`),
-	))
+	app.NewApplication().Run()
 }
